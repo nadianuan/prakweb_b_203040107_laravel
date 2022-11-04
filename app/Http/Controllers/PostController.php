5da6b750bc015946ca75ccc3db159dad1a380cbd
nadianuan <nadia.nuan39@gmail.com>
2022-11-04 21:31:59 +0700

    Searching & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,21 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use App\Pagination\Paginator;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
+        $title = '';
 
-        if(request('search')) {
-            $posts->where('title', 'like', '%' . request('search') . '%');
+        if(request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if(request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
         }
 
         return view('posts', [
-            "title" => "All Post",
+            "title" => "All Post" . $title,
             "active" => 'posts',
-            "posts" => $posts->get()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
